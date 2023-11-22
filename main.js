d6938eb34260b10dacb641de8acee0884e51487d
DEV2DEV-DE <github@dev2dev.de>
2023-11-22 17:41:15 +0100

    Update translations and add username and password fields

--- main.js
+++ main.js
@@ -7,9 +7,7 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require('@iobroker/adapter-core');
-
-// Load your modules here, e.g.:
-// const fs = require("fs");
+const axios = require('axios').default;
 
 class Jablotron extends utils.Adapter {
 
@@ -23,8 +21,6 @@ class Jablotron extends utils.Adapter {
 		});
 		this.on('ready', this.onReady.bind(this));
 		this.on('stateChange', this.onStateChange.bind(this));
-		// this.on('objectChange', this.onObjectChange.bind(this));
-		// this.on('message', this.onMessage.bind(this));
 		this.on('unload', this.onUnload.bind(this));
 	}
 
